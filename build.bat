a6e6b2dde86f650f867f87d0990632e085606be8
Mak <>
2024-10-22 15:19:41 +0200

    [code]: build bat

---
+++ build.bat
@@ -0,0 +1,8 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd C:\GitProjects\godot-jolt
+
+cmake --preset windows-msvc-x64
+
+cmake --build --preset windows-msvc-x64-editor-distribution
+
+cmake --install build/windows-msvc-x64 --config EditorDistribution --prefix "C:\GitProjects\flood"
